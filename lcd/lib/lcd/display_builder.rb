f24972b0ffa40a5361b717481a5903baef2e59ab
Abraham and Erick <erick+abraham@gmail.com>
2014-03-13 11:23:14 -0600

    PASS: RSPEC when number is 0

--- lcd/lib/lcd/display_builder.rb
+++ lcd/lib/lcd/display_builder.rb
@@ -1,6 +1,8 @@
 module LCD
   class DisplayBuilder
 
+    MIN_LINES = 5
+
     attr_reader :lcd_numbers
 
     def initialize(args)
@@ -12,7 +14,51 @@ module LCD
 
     private
     def generate_lcd
+      @lcd_numbers << lcd_body
+    end
 
+    def lcd_body
+      "\n" << top_line << middle_top_line << middle_line << middle_bottom_line << bottom_line
+    end
+
+    def top_line
+      @number.split.reduce('') { |acc, number| acc << number_top_line(number) }
+    end
+
+    def number_top_line(number)
+      " - \n"
+    end
+
+    def middle_top_line
+      @number.split.reduce('') { |acc, number| acc << number_middle_top_line(number) }
+    end
+
+    def number_middle_top_line(number)
+      "| |\n"
+    end
+
+    def middle_line
+      @number.split.reduce('') { |acc, number| acc << number_middle_line(number) }
+    end
+
+    def number_middle_line(number)
+      "   \n"
+    end
+
+    def middle_bottom_line
+      @number.split.reduce('') { |acc, number| acc << number_middle_bottom_line(number) }
+    end
+
+    def number_middle_bottom_line(number)
+      "| |\n"
+    end
+
+    def bottom_line
+      @number.split.reduce('') { |acc, number| acc << number_bottom_line(number) }
+    end
+
+    def number_bottom_line(number)
+      " - \n"
     end
   end
 end
--- lcd/spec/lcd/display_builder_spec.rb
+++ lcd/spec/lcd/display_builder_spec.rb
@@ -5,10 +5,12 @@ describe LCD::DisplayBuilder, '#lcd_numbers' do
   subject { described_class.new(args) }
 
   context "when there is a single number" do
-    let(:args) { { number: 0, size: 1 } }
+    context "when the number is 0" do
+      let(:args) { { number: '0', size: 1 } }
 
-    it "generates the lcd number" do
-      expect(subject.lcd_numbers).to eq(ZERO_SIZE_1)
+      it "generates the lcd number" do
+        expect(subject.lcd_numbers).to eq(ZERO_SIZE_1)
+      end
     end
   end
 end
